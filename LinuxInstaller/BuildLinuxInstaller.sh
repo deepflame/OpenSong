0290ae8533c0269974b8ef3ff0622c3ee2841459
vwout <vwout@7d629990-da18-0410-9c13-aedeb0f471ca>
2010-01-30 23:02:08 +0000

    Update LinuxInstaller to support the new Builds directory structure and new external libraries
    git-svn-id: https://opensong.svn.sourceforge.net/svnroot/opensong/source/trunk@620 7d629990-da18-0410-9c13-aedeb0f471ca

--- LinuxInstaller/BuildLinuxInstaller.sh
+++ LinuxInstaller/BuildLinuxInstaller.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ############
 # Howto use the Linux Install Builder
@@ -7,17 +7,29 @@
 #
 # 2. Do a build of OpenSong using RealBasic,
 #    make sure a valid opensong binary is in the project root.
+#    or in the Builds directory created by REALBasic
 #
 # 3. Run this script, it will generate a .deb package.
 ############
-
+# History
+# Date        Who         What
+# ----------  ----------  --------
+# 2010-01-30  Vwout       Update to support the new Builds directory structure
+#                         and new external libraries
+#
 
 VERSION=`cat ../OpenSong.rbvcp | grep ShortVersion | cut -d = -f 2`
+BASE_DIR=".."
+BUILD_DIR="${BASE_DIR}/Builds - OpenSong.rbvcp/Linux/opensong"
 
-if [ ! -e "../opensong" ]
+if [ ! -e "${BUILD_DIR}/opensong" ]
 then
-  echo "The binary opensong does not exist! Do a build before creating the installer."
-  exit 1
+  BUILD_DIR=$BASE_DIR
+  if [ ! -e "${BUILD_DIR}/opensong" ]
+  then
+    echo "The binary opensong does not exist! Do a build before creating the installer."
+    exit 1
+  fi
 fi
 
 echo "Creating a Debian package for OpenSong v$VERSION"
@@ -35,23 +47,29 @@ mkdir -p opensong/opt/OpenSong/OpenSong\ Defaults/Songs
 mkdir -p opensong/opt/OpenSong/OpenSong\ Languages
 mkdir -p opensong/opt/OpenSong/OpenSong\ Scripture
 mkdir -p opensong/opt/OpenSong/OpenSong\ Settings
+mkdir -p opensong/opt/OpenSong/opensong\ Libs
 mkdir -p opensong/usr/share/applications/
 
-cp ../opensong opensong/opt/OpenSong/
-cp ../changelog.txt opensong/opt/OpenSong/
-cp ../Language-Changelog.txt opensong/opt/OpenSong/
-cp ../Images/ProgramIconPic48-8.xpm opensong/opt/OpenSong/opensong.xpm
-cp ../OpenSong\ Defaults/Backgrounds/* opensong/opt/OpenSong/OpenSong\ Defaults/Backgrounds/
-cp ../OpenSong\ Defaults/Sets/* opensong/opt/OpenSong/OpenSong\ Defaults/Sets/
-cp ../OpenSong\ Defaults/Settings/* opensong/opt/OpenSong/OpenSong\ Defaults/Settings/
-cp ../OpenSong\ Defaults/Songs/* opensong/opt/OpenSong/OpenSong\ Defaults/Songs/
-cp ../OpenSong\ Languages/* opensong/opt/OpenSong/OpenSong\ Languages/
-cp ../OpenSong\ Scripture/KJV opensong/opt/OpenSong/OpenSong\ Scripture/
-cp ../OpenSong\ Settings/bnames.xml opensong/opt/OpenSong/OpenSong\ Settings/
-cp ../OpenSong\ Settings/Globals opensong/opt/OpenSong/OpenSong\ Settings/
-cp ../OpenSong\ Settings/keyconfig.dtd opensong/opt/OpenSong/OpenSong\ Settings/
-cp ../OpenSong\ Settings/style.css opensong/opt/OpenSong/OpenSong\ Settings/
+cp "${BUILD_DIR}"/opensong opensong/opt/OpenSong/
+cp "${BUILD_DIR}"/opensong\ Libs/* opensong/opt/OpenSong/opensong\ Libs/
+cp "${BASE_DIR}"/changelog.txt opensong/opt/OpenSong/
+cp "${BASE_DIR}"/Language-Changelog.txt opensong/opt/OpenSong/
+cp "${BASE_DIR}"/Images/ProgramIconPic48-8.xpm opensong/opt/OpenSong/opensong.xpm
+cp "${BASE_DIR}"/OpenSong\ Defaults/Backgrounds/* opensong/opt/OpenSong/OpenSong\ Defaults/Backgrounds/
+cp "${BASE_DIR}"/OpenSong\ Defaults/Sets/* opensong/opt/OpenSong/OpenSong\ Defaults/Sets/
+cp "${BASE_DIR}"/OpenSong\ Defaults/Settings/* opensong/opt/OpenSong/OpenSong\ Defaults/Settings/
+cp "${BASE_DIR}"/OpenSong\ Defaults/Songs/* opensong/opt/OpenSong/OpenSong\ Defaults/Songs/
+cp "${BASE_DIR}"/OpenSong\ Languages/* opensong/opt/OpenSong/OpenSong\ Languages/
+cp "${BASE_DIR}"/OpenSong\ Scripture/KJV opensong/opt/OpenSong/OpenSong\ Scripture/
+cp "${BASE_DIR}"/OpenSong\ Settings/bnames.xml opensong/opt/OpenSong/OpenSong\ Settings/
+cp "${BASE_DIR}"/OpenSong\ Settings/Globals opensong/opt/OpenSong/OpenSong\ Settings/
+cp "${BASE_DIR}"/OpenSong\ Settings/keyconfig.dtd opensong/opt/OpenSong/OpenSong\ Settings/
+cp "${BASE_DIR}"/OpenSong\ Settings/style.css opensong/opt/OpenSong/OpenSong\ Settings/
 
+chmod +x opensong/opt/OpenSong/opensong
+
+# Create the installer files
+INSTALL_SIZE=`du -sk opensong | cut -f 1`
 
 echo "Package: opensong
 Version: $VERSION
@@ -61,6 +79,7 @@ Architecture: i386
 Essential: no
 Maintainer: Vwout <vwout@users.sourceforge.net>
 Depends: libc6 (>=2.3), libexpat1 (>=1.95.8), libglib2.0-0 (>=2.2.0), libgtk2.0-0 (>=2.2.0)
+Installed-Size: $INSTALL_SIZE
 Description: managing and presenting worship lyrics, chords and more
  OpenSong is a free software application for managing chords and lyrics 
  sheets (lead sheets), presenting lyrics (and custom slides) using a 
@@ -89,9 +108,8 @@ Terminal=false
 Type=Application
 Categories=Multimedia;Office" > opensong/usr/share/applications/opensong.desktop
 
-IFS=$'\n'
-md5sum `find opensong/opt -type f | sed "s/ /\\\ /g"` > opensong/DEBIAN/md5sums
-md5sum `find opensong/usr -type f | sed "s/ /\\\ /g"` >> opensong/DEBIAN/md5sums
+find opensong/opt -type f -print0 | xargs -0 md5sum > opensong/DEBIAN/md5sums
+find opensong/usr -type f -print0 | xargs -0 md5sum >> opensong/DEBIAN/md5sums
 
 if [ -e "opensong_$VERSION-1_i386.deb" ]
 then
